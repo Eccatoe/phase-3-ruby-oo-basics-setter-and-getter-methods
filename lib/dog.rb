64a83b35e30adbd3471e0618e500667e390a7b9c
Elizabeth Catoe <eccatoe2517@gmail.com>
2022-02-23 11:33:33 -0500

    commit tree 223ddb3fa928d4351eea16129505e9874d396606

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,26 @@
+# Define a name getter and a name= setter method for the instance variable @name.
+# Define a breed getter and a breed= setter method for the instance variable @breed.
+
+require 'pry'
+
 class Dog
+    def name=(name)
+        @name=name
+    end
+
+    def name
+        @name
+    end
+
+    def breed=(breed)
+        @breed=breed
+    end
+
+    def breed
+        @breed
+    end
 
 end
+
+binding.pry
+0
